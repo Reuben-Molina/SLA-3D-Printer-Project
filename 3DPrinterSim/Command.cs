f53d38e2441abbf70a84b6cbc8ccd0ada7b8164b
Reuben-Molina <33916135+Reuben-Molina@users.noreply.github.com>
2018-03-20 17:15:52 -0400

    Uploaded Project Files

---
+++ 3DPrinterSim/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DPrinterSim
+{
+    //Bob The Builder
+    class Command
+    {
+        public double z_layer { get; set; }
+        public double x_coordinate { get; set; }
+        public double y_coordinate { get; set; }
+        public bool laser_state { get; set; }
+    }
+}
