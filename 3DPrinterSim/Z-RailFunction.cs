08c6104d25185cdd4d63231e3cae8e79aa96fa7d
Victoria Pohle <victoriapohle@southern.edu>
2018-04-08 22:14:18 -0400

    Z-RailFunctions class added

---
+++ 3DPrinterSim/Z-RailFunction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DPrinterSim
+{
+    class Z_RailFunction
+    {
+
+        public static void GetZCommand()
+        {
+            
+        }
+        public static void UnknownLocation()
+        {
+
+        }
+        public static void Home()
+        {
+
+        }
+        public static void Building()
+        {
+
+        }
+        public static void BuildPlate()
+        {
+
+        }
+    }
+}
